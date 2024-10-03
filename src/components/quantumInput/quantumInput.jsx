4db7cff19d990bf16f74ca9e03fea8f4cd9c87e8
Artem Fedchenko <artem_fedchenko_2017@mail.ru>
2024-10-03 20:26:49 +0700

    feat: new quantum input block

--- src/components/quantumInput/quantumInput.jsx
+++ src/components/quantumInput/quantumInput.jsx
@@ -1,7 +1,7 @@
 import st from "./quantumInput.module.css";
-import inputIcon from "./assets/inputIcon.svg";
 import addIcon from "./assets/addIcon.svg";
-import { useState, useEffect, useRef } from "react";
+import cpuIcon from "./assets/CPU.svg";
+import { useState, useEffect } from "react";
 import TasksStorage from "../../store/taskStorage";
 import { observer } from "mobx-react-lite";
 
@@ -10,29 +10,16 @@ export const QuantumInput = observer(() => {
 	const [attention, setAttention] = useState(false);
 	const [isAddingQuantum, setIsAddingQuantum] = useState(false);
 
-	const inputRef = useRef(null);
-
 	useEffect(() => {
 		setWarning(quantum);
-		//при нажатии enter сохраняем квант в хранилище
-		const listener = (event) => {
-			if (
-				document.activeElement === inputRef.current &&
-				(event.code === "Enter" || event.code === "NumpadEnter")
-			) {
-				event.preventDefault();
-				setQuantumStorage();
-			}
-		};
-		document.addEventListener("keydown", listener);
-		return () => {
-			document.removeEventListener("keydown", listener);
-		};
 	}, [quantum]);
 
 	//устанавливаем значение кванта в хранилище
-	const setQuantumStorage = () => {
+	const setQuantumStorage = (e) => {
 		if (!attention) {
+			//останавливаем всплытие события, чтобы внешняя кнопка не срабатывала
+			//isAddingQuantum необходим, чтобы событие не останавливалось при сложенной кнопке
+			isAddingQuantum && e?.stopPropagation();
 			TasksStorage.quantum = quantum;
 			//пересчитываем максимально возможное количество очередей
 			TasksStorage.setMaxQueueQuantity();
@@ -40,16 +27,16 @@ export const QuantumInput = observer(() => {
 		}
 	};
 
-	//устанавливаем состояние кванта в форме
-	const SetQuantum = (quantumValue) => {
-		setQuantum(quantumValue);
-	};
 	//показываем предупреждение o кванте <= 0
 	const setWarning = (quantumValue) => {
 		if (quantumValue <= 0) setAttention(true);
 		else setAttention(false);
 	};
 
+	const keyDownHandler = (e) => {
+		if (e.code === "Escape") setIsAddingQuantum(false);
+		else if (e.code === "Enter" || e.code === "NumpadEnter") setQuantumStorage(e);
+	};
 	return (
 		<div className={st.quantum_block}>
 			<button
@@ -59,34 +46,32 @@ export const QuantumInput = observer(() => {
 				{!isAddingQuantum && <p>Set quantum</p>}
 				{isAddingQuantum && (
 					<input
-						ref={inputRef}
+						autoFocus
+						min={1}
+						onKeyDown={keyDownHandler}
 						type="number"
 						className={st.quantum_input}
 						placeholder="Enter quantum"
 						onChange={(e) => {
-							SetQuantum(Number(e.target.value));
+							setQuantum(Number(e.target.value));
 						}}
 					/>
 				)}
-				<img className={st.addIcon} src={addIcon} alt="Add quantum" />
+				<button onClick={(e) => setQuantumStorage(e)}>
+					<img className={st.addIcon} src={addIcon} alt="Add quantum" />
+				</button>
 			</button>
-
-			{/* <button
-					onClick={() => {
-						setQuantumStorage();
-					}}
-					className={st.submitBtn}
-				>
-					<img className={st.submitImg} src={inputIcon} alt="Записать квант времени" />
-				</button> */}
-			<div className={st.currentQuantum}>
-				<p className={st.txt}>{TasksStorage.quantum}</p>
+			<div className={st.quantum}>
+				<div title="Current quantum" className={st.currentQuantum}>
+					<p className={st.txt}>{TasksStorage.quantum}</p>
+				</div>
+				<img title="Current quantum" className={st.cpuIcon} src={cpuIcon} alt="Quantum" />
 			</div>
-			{/* {attention && (
+			{attention && (
 				<p className={`${st.txt} ${st.attentionTxt}`}>
 					*The quantum must be greater than zero
 				</p>
-			)} */}
+			)}
 		</div>
 	);
 });
